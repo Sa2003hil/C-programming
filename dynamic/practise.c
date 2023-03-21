1ada43a25994058574923a2185e4c488c4bb86ef
Sahil <Sahil2003@gmail.com>
2023-03-21 23:00:02 +0530

    coding files of 21 march

--- dynamic/practise.c
+++ dynamic/practise.c
@@ -1,50 +1,46 @@
-// C program to count the number of
-// vowels and consonants in a string
-
 #include <stdio.h>
-#include <string.h>
 
-// Function to count number
-// of vowels and consonant
-void count_vowels_and_consonant(char *str)
+int main(void)
 {
-    // Declare the variable vowels and consonant
-    int vowels = 0, consonants = 0;
-
-    int i;
-    char ch;
-
-    // Take each character from this string to check
-    for (i = 0; str[i] != '\0'; i++)
+    // your code goes here
+    int t;
+    scanf("%d", &t);
+    while (t--)
     {
+        int n;
+        scanf("%d", &n);
 
-        ch = str[i];
+        char str[n];
+        scanf("%s", str);
+        int count1 = 0, count0 = 0;
 
-        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
-            vowels++;
+        for (int i = 0; i < n; i++)
+        {
+            if (str[i] == '1')
+            {
+                count1++;
+            }
+            else if (str[i] == '0')
+            {
+                count0++;
+            }
+        }
+        // printf("%d %d", count0, count1);
 
-        else if (ch == ' ')
-            continue;
+        if (count1 > count0)
+        {
+            printf("%d\n", count1 - count0 + 2);
+        }
+        else if (count0 > count1)
+        {
+            printf("%d\n", count1 + 1);
+        }
 
-        else
-
-            consonants++;
+        else if (count1 = count0)
+        {
+            printf("%d\n", n);
+        }
     }
-
-    // Print the total count of vowels and consonants
-    printf("\nVowels: %d", vowels);
-    printf("\nConsonants: %d", consonants);
-}
-
-// Driver function.
-int main()
-{
-    // char *str = "geeks for geeks";
-    // printf("String: %s", str);
-    char str[100];
-    fgets(str, 100, stdin);
-
-    count_vowels_and_consonant(str);
 
     return 0;
 }
