5ed19e4bc483eeed356c0580bc0d6a1cc2cfcff6
Sahil <Sahil2003@gmail.com>
2023-04-08 00:15:54 +0530

    new files of 8 april

--- Questions/armstrong.c
+++ Questions/armstrong.c
@@ -20,7 +20,7 @@ int main()
     if (result == num)
         printf("%d is an Armstrong number.", num);
     else
-        printf("%d is not an Armstrong number.", num);
+        printf("%d is  not an Armstrong number.", num);
 
     return 0;
 }
